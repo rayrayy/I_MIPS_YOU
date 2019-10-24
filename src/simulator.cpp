99ad9b7957977b4756cf7b49d64720775adcd79b
Rayrayy <raymond.zhu@live.se>
2019-10-24 19:03:10 +0100

    Groundwork Laid Out

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -1,30 +1,36 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 int main(int argc, char *argv[]){
-  std::string binName = arg[1];
 
-  std::cerr << binName;
+    std::string binName = argv[1];
 
-  std::ifstream binStream(argv[1], std::ios::binary);
+    std::cerr << binName;
 
-  binSTream.seek(seek_end);
-  sizeBin=binStream.tell();
 
-  std::vector<uint32_t> imem;
-  imem.resize(0x1000000);
+    std::ifstream binStream(argv[1], std::ios::binary);
 
-  std::vector<uint32_t> dmem;
-  dmem.resize(0x4000000);
+    // binStream.seek(seek_end);
+    // sizeBin=binStream.tell();
 
-  binStream.read(&tmp[0], sizeBin);
+    std::vector<uint32_t> imem;
+    imem.resize(0x1000000);
 
-  uint32_t registers[32]={0};
-  uint32_t pc=IMEM_OFFSET;
+    std::vector<uint32_t> dmem;
+    dmem.resize(0x4000000);
 
-  while(1){
-    uint32_t instruction;
-    
-  }
+    // binStream.read(&tmp[0], sizeBin);
+
+    uint32_t IMEM_OFFSET = 0x1000000;
+    uint32_t registers[32]={0};
+    uint32_t pc=IMEM_OFFSET;
+
+    while(1){
+      uint32_t instruction;
+    }
+
+    return(0);
+
 }
