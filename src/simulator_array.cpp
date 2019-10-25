d757211f7c789559c8167ec9f65790014eb2e69a
Rayrayy <raymond.zhu@live.se>
2019-10-25 18:59:00 +0100

    [NOT FIXED] Issues with converting binary data from .bin to uint32_t

--- src/simulator_array.cpp
+++ src/simulator_array.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include <string>
 
 int main(int argc, char *argv[]){
 
@@ -10,16 +9,14 @@ int main(int argc, char *argv[]){
 
     std::string binName = argv[1];
 
-    // std::cout << binName;
+    std::cout << binName;
 
     // Initializing array and offset for instruction memory
-    std::vector<uint32_t> imem;
-    imem.resize(0x1000000);
+    uint32_t imem[0x1000000] = {0};
     uint32_t IMEM_OFFSET = 0x10000000;
 
     //Initializing array and offset for data memory
-    std::vector<uint32_t> dmem;
-    dmem.resize(0x4000000);
+    uint32_t dmem[0x4000000] = {0};
     uint32_t DMEM_OFFSET = 0x20000000;
 
     //Initializing all registers at 0x00000000
@@ -30,17 +27,17 @@ int main(int argc, char *argv[]){
 
 
     // Opening the binary file
-    std::ifstream binStream;
-    binStream.open(binName, std::ios::binary);
+    // std::ifstream binStream;
+    // binStream.open(binName, std::ios::binary);
 
-    if (binStream.is_open()){
-      std::cout << "Binary opened successfully." << std::endl;
-      std::string line;
-      while (getline(binStream, line)) {
-        std::cout << typeid(line).name() << std::endl; //this line needs to change to one which loads the binary value into the instruction memory.
-      }
-      binStream.close();
-    }
+    // if (binStream.is_open()){
+    //   std::cout << "Binary opened successfully." << std::endl;
+    //   std::string line;
+    //   while (getline(binStream, line)) {
+    //       std::cout << line.c_str() << std::endl;
+    //   }
+    //   binStream.close();
+    // }
 
     // else{
     //   std::cout << "Error" << std::endl;
