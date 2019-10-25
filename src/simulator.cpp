48eba8d73f6c8db7e801df69661d1a87b59b4f38
Rayrayy <raymond.zhu@live.se>
2019-10-25 16:14:11 +0100

    Input file read successful.

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -7,19 +7,27 @@ int main(int argc, char *argv[]){
 
     std::string binName = argv[1];
 
-    std::cerr << binName;
 
 
-    std::ifstream binStream(argv[1], std::ios::binary);
+    std::ifstream binStream(binName, std::ios::binary);
+
+    if (binStream.is_open()){
+      std::cerr << "File is open" << std::endl;
+    }
+    else{
+      std::cerr << "Error" << std::endl;
+      return(-1);
+    }
+
 
     // binStream.seek(seek_end);
     // sizeBin=binStream.tell();
 
-    std::vector<uint32_t> imem;
-    imem.resize(0x1000000);
-
-    std::vector<uint32_t> dmem;
-    dmem.resize(0x4000000);
+    // std::vector<uint32_t> imem;
+    // imem.resize(0x1000000);
+    //
+    // std::vector<uint32_t> dmem;
+    // dmem.resize(0x4000000);
 
     // binStream.read(&tmp[0], sizeBin);
 
@@ -29,6 +37,8 @@ int main(int argc, char *argv[]){
 
     while(1){
       uint32_t instruction;
+      std::cerr << "Hello World" << std::endl;
+      return(0);
     }
 
     return(0);
