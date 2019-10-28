2c18253d063379be1162dd45d498f7f5a83d52c6
rayrayy <b.pulmannova@gmail.com>
2019-10-28 13:42:52 +0100

    small organization changes

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -4,32 +4,16 @@
 #include <vector>
 //using namespace std;
 
-#define IMEM_OFFSET 0x100000
-#define DMEM_OFFSET 0x20000000
+#define ADDR_INSTR_OFFSET 0x100000
+#define ADDR_DATA_OFFSET 0x20000000
 
 int main(int argc, char *argv[]){
 
     (void)argc; //to stop the warning of not using this argument
     std::string binName = argv[1];
 
-    //Initializing array and offset for instruction memory
-    // uint32_t imem[0x1000000] = {0};
-    // uint32_t IMEM_OFFSET = 0x10000000;
-    //
-    // //Initializing array and offset for data memory
-    // uint32_t dmem[0x4000000] = {0};
-    // uint32_t DMEM_OFFSET = 0x20000000;
-    //
-    // //Initializing all registers at 0x00000000
-    // uint32_t registers[32]={0};
-    //
-    // //Initializing pc at IMEM_OFFSET
-    // uint32_t pc=IMEM_OFFSET;
-
-
     //Opening the binary file
-    std::ifstream binStream(binName.c_str(), std::ios::in | std::ios::binary | std::ios::ate); //opens binary file for read with get pointer at the end
-
+    std::ifstream binStream(binName, std::ios::in | std::ios::binary | std::ios::ate); //opens binary file for read with get pointer at the end
     std::streampos binStream_size;
     char *memBlock;
 
@@ -39,13 +23,16 @@ int main(int argc, char *argv[]){
     binStream.seekg (0, std::ios::beg);
     binStream.read (memBlock, binStream_size);
     binStream.close();
+    delete[] memBlock;
     }
     else{
       std::cerr << "Unable to open file" << std::endl;
-      return(-1);
+      return(-1); //is this the right return value?
     }
 
-    std::cerr << memBlock[34] << std::endl;
+    std::vector<uint32_t> instrMem;
+    std::vector<uint32_t> dataMem;
+    
 
 
 
